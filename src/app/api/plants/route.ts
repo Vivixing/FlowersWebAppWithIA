2e6a824b033f679b85c95652110f49471fda1278
Vivixing <echeverri.vivian1120@gmail.com>
2025-05-29 15:41:29 -0500

    Añadido import de Openai

--- src/app/api/plants/route.ts
+++ src/app/api/plants/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
 import { ObjectId } from "mongodb";
 import { PlantResponse } from "@/Interfaces/plant";
-
+import clientPromise from "@/lib/mongodb";
+import OpenAI from "openai";
